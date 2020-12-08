3b963c4a48db5101550dcab8be4d16acadf909f9
Mark Dore <dorem83@gmail.com>
2020-12-08 21:11:45 +0000

    adding dynamic styles and classes

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'yellow',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -61,11 +62,23 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red'; 
     } 
+
+    const classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>YO</h1>
-        <p>This is really working</p>
+
+        <p className={classes.join(' ')}>This is really working</p>
 
         <button 
           style={style}
